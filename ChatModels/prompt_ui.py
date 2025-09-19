ca6f668dcb5a8511cd99edec761b8027ea1941a6
Anisha <anishaworks21@gmail.com>
2025-09-19 22:19:53 +0530

    prompts

--- ChatModels/prompt_ui.py
+++ ChatModels/prompt_ui.py
@@ -1,13 +1,23 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
 from dotenv import load_dotenv
 
-load_dotenv()
+import streamlit as st;
+
+load_dotenv();
+
 
 llm = HuggingFaceEndpoint(
     repo_id="HuggingFaceH4/zephyr-7b-beta",  # chat-tuned model
     task="text-generation"
-) 
+)
 
 model = ChatHuggingFace(llm=llm)
-result = model.invoke("Explain the theory of relativity in simple terms.")
-print(result.content)
+st.header('Research Tool')
+input=st.text_input(
+    'Enter your prompt'
+);
+
+if st.button('Summarize'):
+    result = model.invoke(input)
+    st.write(result.content)
